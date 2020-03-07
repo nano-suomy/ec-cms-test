7db55bb4393b0133853ab20bc5c33d5549d49520
nano_suomy <nano.suomy1x383@gmail.com>
2020-03-08 03:41:21 +0900

    minor fix

--- server/resources/views/admin/goods/show.blade.php
+++ server/resources/views/admin/goods/show.blade.php
@@ -1,26 +1,27 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h1 class="item-head">{{$item->title}}</h1>
-                </div>
-                <div class="panel-body">
-                    <div class="item-container">
-                        <div class="img-container">
-                            <img class="item-img" alt="item" src="{{ asset('/storage/img/'.$item->image) }}">
-                        </div>
-                        <div class="detail-container">
-                            <div class="item-price">
-                                <h2>価格:</h2>
-                                <p>{{$item->price}}円</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h1 class="item-head">{{$item->title}}</h1>
+                    </div>
+                    <div class="panel-body">
+                        <div class="item-container">
+                            <div class="img-container">
+                                <img class="item-img" alt="item" src="{{ asset('/storage/img/'.$item->image) }}">
                             </div>
-                            <div class="item-body">
-                                <h2>詳細</h2>
-                                <p>{{$item->description}}</p>
+                            <div class="detail-container">
+                                <div class="item-price">
+                                    <p>発売日: {{ $item->created_at->format('Y年m月d日') }}</p>
+                                    <p>{{$item->price}}円</p>
+                                </div>
+                                <div class="item-body">
+                                    <h2>詳細</h2>
+                                    <p>{{$item->description}}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -28,5 +29,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
